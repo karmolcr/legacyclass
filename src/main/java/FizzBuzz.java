6ff580ae29e8d947ce76e7332d5e1fe6b1efa89d
aaeang <aaeang@hotmail.com>
2017-03-22 16:03:54 +0700

    Inline temp parameter

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -10,23 +10,15 @@ public class FizzBuzz {
     public String answer(int input) {
 
 
-        Rule rule_01 = new FizzBuzzRule();
-        Rule rule_02 = new FizzRule();
-        Rule rule_03 = new BuzzRule();
+        List<Rule> rules = Arrays.asList(new FizzBuzzRule(), new FizzRule(), new BuzzRule());
 
-        List<Rule> rules = Arrays.asList(rule_01, rule_02, rule_03);
-
-        if(rules.get(0).isAnswer(input)){
-            return rules.get(0).getAnswer();
+        for (Rule rule:
+             rules) {
+            if(rule.isAnswer(input)){
+                return rule.getAnswer();
+            }
         }
 
-        if(rules.get(1).isAnswer(input)){
-            return rules.get(1).getAnswer();
-        }
-
-        if(rules.get(2).isAnswer(input)){
-            return rules.get(2).getAnswer();
-        }
         return Integer.toString(input);
     }
 
