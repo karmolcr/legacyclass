a6250db918c76ae3b85c9e51549cc3a5ff664002
aaeang <aaeang@hotmail.com>
2017-03-22 12:08:45 +0700

    Refactor code

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -3,6 +3,8 @@ public class FizzBuzz {
     public String answer(int input) {
         if(input == 3){
             return "Fizz";
+        }else if(input == 5){
+            return "Buzz";
         }
         return Integer.toString(input);
     }
