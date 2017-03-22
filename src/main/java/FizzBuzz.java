6469adba94f714ad2fde5e8439aa132eca004cb9
aaeang <aaeang@hotmail.com>
2017-03-22 13:16:59 +0700

    Add logic and test for input value = 7 - 10

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,10 +1,10 @@
 public class FizzBuzz {
 
     public String answer(int input) {
-        if(input == 3 || input == 6){
+        if(input%3 == 0){
             return "Fizz";
         }
-        if(input == 5){
+        if(input%5 == 0){
             return "Buzz";
         }
         return Integer.toString(input);
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -29,7 +29,11 @@ public class FizzBuzzTest {
                 {3, "Fizz"},
                 {4, "4"},
                 {5, "Buzz"},
-                {6, "Fizz"}
+                {6, "Fizz"},
+                {7,"7"},
+                {8,"8"},
+                {9,"Fizz"},
+                {10,"Buzz"}
         });
     }
 
