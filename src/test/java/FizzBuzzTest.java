37f3b5a060a29270c38c97dc67f065a0dbfbd943
aaeang <aaeang@hotmail.com>
2017-03-22 10:39:31 +0700

    Refactor test code

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,17 +1,23 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static junit.framework.Assert.assertEquals;
 
 public class FizzBuzzTest {
+
+    private FizzBuzz game;
+
+    @Before
+    public void setup(){
+        game = new FizzBuzz();
+    }
     @Test
     public void oneShouldBe1(){
-        FizzBuzz game = new FizzBuzz();
         assertEquals("1", game.answer(1));
     }
 
     @Test
     public void twoShouldBe2(){
-        FizzBuzz game = new FizzBuzz();
         assertEquals("2", game.answer(2));
     }
 }
