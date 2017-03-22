2b1a1412b0aa33dbed1db81236adceed194401b6
aaeang <aaeang@hotmail.com>
2017-03-22 09:56:14 +0700

    First Commit Legacy Code

---
+++ src/test/java/FizzBuzzTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertFalse;
+
+/**
+ * Created by saavik on 3/22/2017 AD.
+ */
+public class FizzBuzzTest {
+    @Test
+    public void oneShouldBe1(){
+        assertFalse(true);
+    }
+}
