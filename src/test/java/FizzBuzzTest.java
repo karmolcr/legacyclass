7e759f2b50a80642b98f96aa52402b6011575e88
aaeang <aaeang@hotmail.com>
2017-03-22 11:45:09 +0700

    Refactor test using Parameterize

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,23 +1,41 @@
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 import static junit.framework.Assert.assertEquals;
 
+@RunWith(Parameterized.class)
 public class FizzBuzzTest {
 
     private FizzBuzz game;
+
+    private int input;
+    private String expected;
+
+    public FizzBuzzTest(int input,String expected){
+        this.input = input;
+        this.expected = expected;
+    }
+
+    @Parameterized.Parameters
+    public static Collection intitalParam() {
+        return Arrays.asList(new Object[][] {
+                { 1, "1" },
+                { 2, "2" }
+        });
+    }
 
     @Before
     public void setup(){
         game = new FizzBuzz();
     }
     @Test
-    public void oneShouldBe1(){
-        assertEquals("1", game.answer(1));
+    public void checkAnswer(){
+        assertEquals(expected, game.answer(input));
     }
 
-    @Test
-    public void twoShouldBe2(){
-        assertEquals("2", game.answer(2));
-    }
 }
